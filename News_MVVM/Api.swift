d3db67ee48b0423c29d6faec1f27ee5aef18c423
DJDrama <putzzang@hotmail.com>
2020-10-12 14:25:36 +0900

    Initial Commit

---
+++ News_MVVM/Api.swift
@@ -0,0 +1,13 @@
+//
+//  Api.swift
+//  News_MVVM
+//
+//  Created by jb on 2020/10/12.
+//
+
+import Foundation
+
+struct API {
+    static let key = "get key from newsapi.org"
+    
+}
---
+++ News_MVVM/Views/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  News_MVVM
+//
+//  Created by jb on 2020/10/12.
+//
+
+\
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
